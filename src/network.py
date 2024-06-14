4c9d789507bd7f2ac665f706cc3eff5a33187a16
Jogjun <truthjun1234@gmail.com>
2024-06-14 13:21:09 +0000

    commit

--- src/network.py
+++ src/network.py
@@ -9,30 +9,138 @@ from lightning.pytorch.loggers.wandb import WandbLogger
 from torch import nn
 from torchvision import models
 from torchvision.models.alexnet import AlexNet
+import torch.nn.functional as F
 import torch
 
 # Custom packages
-from src.metric import MyAccuracy
+from src.metric import MyAccuracy, MyF1Score
 import src.config as cfg
 from src.util import show_setting
 
 
-# [TODO: Optional] Rewrite this class if you want
+class ChannelAttention(nn.Module):
+    def __init__(self, num_channels):
+        super(ChannelAttention, self).__init__()
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.fc = nn.Sequential(
+            nn.Linear(num_channels, num_channels // 8, bias=False),
+            nn.ReLU(inplace=True),
+            nn.Linear(num_channels // 8, num_channels, bias=False),
+            nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        b, c, _, _ = x.size()
+        y = self.avg_pool(x).view(b, c)
+        y = self.fc(y).view(b, c, 1, 1)
+        return x * y.expand_as(x)
+    
+class RecurrentConvLayer(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, stride=1, padding=0, num_recurrences=2):
+        super(RecurrentConvLayer, self).__init__()
+        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding)
+        self.recurrent_conv = nn.Conv2d(out_channels, out_channels, kernel_size=1)  # 1x1 convolution
+        self.num_recurrences = num_recurrences
+
+    def forward(self, x):
+        x = F.relu(self.conv(x))
+        for _ in range(self.num_recurrences):
+            rec_out = F.relu(self.recurrent_conv(x))
+            x = rec_out + x  # Avoid in-place operation
+        return x
+    
 class MyNetwork(AlexNet):
-    def __init__(self):
+    def __init__(self, num_classes = 200):
         super().__init__()
+        
+        self.num_classes = num_classes
+            # Replace the original features of AlexNet with a new set of layers
+        self.features = nn.Sequential(
+            RecurrentConvLayer(3, 64, kernel_size=11, stride=4, padding=2),
+            # nn.Conv2d(3, 64, kernel_size=11, stride=4, padding=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Dropout(0.25),  # Dropout 추가
+            
+            nn.Conv2d(64, 192, kernel_size=5, padding=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Dropout(0.25),  # Dropout 추가
 
-        # [TODO] Modify feature extractor part in AlexNet
-
+            nn.Conv2d(192, 384, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(384, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+        )
+        
+    #     # Attention Layer
+        self.attention = ChannelAttention(256)  # 가정으로 features의 마지막 출력 채널을 256으로 설정
+        
+        # Replace the original classifier of AlexNet
+        self.classifier = nn.Sequential(
+            nn.Dropout(0.5),
+            nn.Linear(256 * 6 * 6, 4096),  # 입력 특징의 수는 레이어와 입력 크기에 따라 달라질 수 있음
+            nn.ReLU(inplace=True),
+            nn.Dropout(0.5),
+            nn.Linear(4096, 4096),
+            nn.ReLU(inplace=True),
+            nn.Dropout(0.5),
+            nn.Linear(4096, self.num_classes),
+        )
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        # [TODO: Optional] Modify this as well if you want
-        x = self.features(x)
-        x = self.avgpool(x)
+        # Initial feature extraction
+        features = self.features(x)
+        # Apply attention
+        features = self.attention(features)
+        
+        # Continue through the network
+        x = self.avgpool(features)
         x = torch.flatten(x, 1)
         x = self.classifier(x)
         return x
 
+
+    # def forward(self, x: torch.Tensor) -> torch.Tensor:
+    #     # [TODO: Optional] Modify this as well if you want
+    #     x = self.features(x)
+    #     x = self.avgpool(x)
+    #     x = torch.flatten(x, 1)
+    #     x = self.classifier(x)
+    #     return x
+
+    
+class AlexNetWithRCL(nn.Module):
+    def __init__(self, num_classes=200):
+        super(AlexNetWithRCL, self).__init__()
+        self.features = nn.Sequential(
+            RecurrentConvLayer(3, 64, kernel_size=11, stride=4, padding=2),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Conv2d(64, 192, kernel_size=5, padding=2),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Conv2d(192, 384, kernel_size=3, padding=1),
+            nn.Conv2d(384, 256, kernel_size=3, padding=1),
+            nn.Conv2d(256, 256, kernel_size=3, padding=1),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+        )
+        self.classifier = nn.Sequential(
+            nn.Dropout(0.5),
+            nn.Linear(256 * 6 * 6, 4096),
+            nn.ReLU(inplace=True),
+            nn.Dropout(0.5),
+            nn.Linear(4096, 4096),
+            nn.ReLU(inplace=True),
+            nn.Linear(4096, num_classes),
+        )
+
+    def forward(self, x):
+        x = self.features(x)
+        x = torch.flatten(x, 1)
+        x = self.classifier(x)
+        return x
 
 class SimpleClassifier(LightningModule):
     def __init__(self,
@@ -46,6 +154,8 @@ class SimpleClassifier(LightningModule):
         # Network
         if model_name == 'MyNetwork':
             self.model = MyNetwork()
+        elif model_name == 'AlexNetWithRCL':
+            self.model = AlexNetWithRCL()
         else:
             models_list = models.list_models()
             assert model_name in models_list, f'Unknown model name: {model_name}. Choose one from {", ".join(models_list)}'
@@ -56,6 +166,7 @@ class SimpleClassifier(LightningModule):
 
         # Metric
         self.accuracy = MyAccuracy()
+        self.f1_score = MyF1Score()
 
         # Hyperparameters
         self.save_hyperparameters()
@@ -78,15 +189,19 @@ class SimpleClassifier(LightningModule):
 
     def training_step(self, batch, batch_idx):
         loss, scores, y = self._common_step(batch)
+        self.f1_score.update(scores, y)
+        f1 = self.f1_score.compute()
         accuracy = self.accuracy(scores, y)
-        self.log_dict({'loss/train': loss, 'accuracy/train': accuracy},
+        self.log_dict({'loss/train': loss, 'accuracy/train': accuracy, 'f1_score/train':f1},
                       on_step=False, on_epoch=True, prog_bar=True, logger=True)
         return loss
 
     def validation_step(self, batch, batch_idx):
         loss, scores, y = self._common_step(batch)
+        self.f1_score.update(scores, y)
+        f1 = self.f1_score.compute()
         accuracy = self.accuracy(scores, y)
-        self.log_dict({'loss/val': loss, 'accuracy/val': accuracy},
+        self.log_dict({'loss/val': loss, 'accuracy/val': accuracy, 'f1_score/train':f1},
                       on_step=False, on_epoch=True, prog_bar=True, logger=True)
         self._wandb_log_image(batch, batch_idx, scores, frequency = cfg.WANDB_IMG_LOG_FREQ)
 
